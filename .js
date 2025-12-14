d6ed3ae0c0b28e472484ade976e4e08f5579a893
lhk <lhk@qq.com>
2025-12-14 14:36:28 +0800

    1

---
+++ .js
@@ -0,0 +1,12 @@
+function test(a, b) {
+    return a + b
+}
+function daysBetweenDates(date1, date2) {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(date1);
+    const secondDate = new Date(date2);
+
+    const diffDays = Math.round(Math.abs(firstDate - secondDate) / oneDay);
+    return diffDays + 1;
+}
+const a = test(3, 2)
